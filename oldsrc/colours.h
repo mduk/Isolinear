2cb9d2917efb4d7a247c3e41399fe28b1d261b96
Daniel Kendell <daniel.kendell@gmail.com>
2021-10-11 16:13:33 +0100

    pretty sure i don't need any of that anymore

--- oldsrc/colours.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstdlib>
-#include <vector>
-
-typedef u_int32_t Colour;
-
-Colour RandomColour();
--- oldsrc/sdlcreate.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <SDL2/SDL.h>
-
-SDL_Window* createWindow(int w, int h);
-SDL_Renderer* createRenderer(SDL_Window* window);
-
