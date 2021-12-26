0fb356192a726b0dfffb67b0bc50a84fca9ffb1d
Daniel Kendell <daniel@kendell.org.uk>
2021-12-26 16:39:31 +0000

    ui

--- include/isolinear/ui.h
+++ include/isolinear/ui.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <list>
+#include <map>
+#include <string>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
@@ -124,6 +126,153 @@ namespace isolinear::ui {
 
       void OnPointerEvent(PointerEvent event) {
         emit signal_press();
+      }
+  };
+
+
+  class ButtonBar : public drawable {
+    protected:
+      Grid grid;
+      Window& window;
+      std::map<std::string, isolinear::ui::button> buttons;
+      Vector2D button_size{2,2};
+
+    public:
+      ButtonBar(Window& w, Grid g)
+        : window{w}, grid{g}
+      {};
+
+      virtual Region2D ButtonRegion(int i) const = 0;
+      virtual Region2D BarRegion() const = 0;
+
+      virtual ColourScheme Colours() const {
+        return drawable::Colours();
+      }
+
+      virtual void Colours(ColourScheme cs) {
+        for (auto& [label, button] : buttons) {
+          button.Colours(cs);
+        }
+        drawable::Colours(cs);
+      }
+
+      virtual isolinear::ui::button& AddButton(std::string label) {
+        buttons.try_emplace(
+            label,
+              window,
+              ButtonRegion(buttons.size() + 1),
+              label
+          );
+        return buttons.at(label);
+      }
+
+      virtual isolinear::ui::button& GetButton(std::string label) {
+        return buttons.at(label);
+      }
+
+      virtual void DeactivateAll() {
+        for (auto& [label, button] : buttons) {
+          button.Deactivate();
+        }
+      }
+
+      virtual void ActivateOne(std::string label) {
+        buttons.at(label).Activate();
+      }
+
+      virtual void OnPointerEvent(PointerEvent event) override {
+        for (auto& [label, button] : buttons) {
+          if (button.Bounds().Encloses(event.Position())) {
+            button.OnPointerEvent(event);
+            return;
+          }
+        }
+      };
+
+      virtual int Height() const {
+        return grid.MaxRows();
+      }
+
+      virtual int Width() const {
+        return grid.MaxColumns();
+      }
+
+      virtual Region2D Bounds() const override {
+        return grid.bounds;
+      }
+
+      void Draw(SDL_Renderer* renderer) const override {
+        for (auto const& [label, button] : buttons) {
+          button.Draw(renderer);
+        }
+
+        Region2D bar = BarRegion();
+
+        boxColor(renderer,
+            bar.NearX(), bar.NearY(),
+            bar.FarX(),  bar.FarY(),
+            Colours().frame
+          );
+      }
+  };
+
+
+  class horizontal_button_bar : public ButtonBar {
+    public:
+      horizontal_button_bar(Window& w, Grid g) : ButtonBar(w, g) {}
+
+      Region2D ButtonRegion(int i) const override {
+        int near_col = button_size.x * (i-1) + 1;
+        int near_row = 1;
+        int  far_col = button_size.x * i;
+        int  far_row = grid.MaxRows();
+
+        return grid.CalculateGridRegion(
+            near_col, near_row,
+             far_col, far_row
+          );
+      }
+
+      Region2D BarRegion() const override {
+        int near_col = button_size.x * buttons.size() + 1;
+        int near_row = 1;
+        int  far_col = grid.MaxColumns();
+        int  far_row = grid.MaxRows();
+
+        return grid.CalculateGridRegion(
+            near_col, near_row,
+             far_col, far_row
+          );
+      }
+  };
+
+
+  class vertical_button_bar : public ButtonBar {
+    public:
+      vertical_button_bar(Window& w, Grid g) : ButtonBar(w, g) {}
+
+      Region2D ButtonRegion(int i) const override {
+        int near_col = 1;
+        int near_row = button_size.y * (i-1) + 1;
+        int  far_col = grid.MaxColumns();
+        int  far_row = button_size.y * i;
+
+        return grid.CalculateGridRegion(
+            near_col, near_row,
+             far_col, far_row
+          );
+      }
+
+      Region2D BarRegion() const override {
+        int near_col = 1;
+        int near_row = button_size.y * buttons.size() + 1;
+        int  far_col = grid.MaxColumns();
+        int  far_row = grid.MaxRows();
+
+        return grid.CalculateGridRegion(
+            near_col, near_row,
+             far_col, far_row
+          );
       }
   };
 
