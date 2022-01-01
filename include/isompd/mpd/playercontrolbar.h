bffc6f85907224409cfb40b8f47cd94571acf685
Daniel Kendell <daniel@kendell.org.uk>
2022-01-01 15:43:23 +0000

    playercontrolbar

--- include/isompd/mpd/playercontrolbar.h
+++ include/isompd/mpd/playercontrolbar.h
@@ -28,10 +28,6 @@ class PlayerControlBar : public horizontal_button_bar {
     isolinear::ui::button& btnStop;
     isolinear::ui::button& btnPrevious;
     isolinear::ui::button& btnNext;
-    isolinear::ui::button& btnConsume;
-    isolinear::ui::button& btnRandom;
-    isolinear::ui::button& btnSingle;
-    isolinear::ui::button& btnRepeat;
 
     int queue_length = 0;
 
@@ -44,17 +40,9 @@ class PlayerControlBar : public horizontal_button_bar {
       , btnStop(AddButton("STOP"))
       , btnPrevious(AddButton("PREVIOUS"))
       , btnNext(AddButton("NEXT"))
-      , btnConsume(AddButton("CONSUME"))
-      , btnRandom(AddButton("RANDOM"))
-      , btnSingle(AddButton("SINGLE"))
-      , btnRepeat(AddButton("REPEAT"))
     {
       miso::connect(mpd.signal_status, [this](mpdxx::status status){
         cout << fmt::format("PlayerControlBar signal_status begin\n");
-        btnConsume.Active(status.Consume());
-        btnRandom.Active(status.Random());
-        btnSingle.Active(status.Single());
-        btnRepeat.Active(status.Repeat());
 
         if (status.IsPlaying()) {
           cout << " => Playing\n";
@@ -108,10 +96,6 @@ class PlayerControlBar : public horizontal_button_bar {
       miso::connect(btnPrevious.signal_press, [this]() { mpd.Previous();      });
       miso::connect(    btnNext.signal_press, [this]() { mpd.Next();          });
       miso::connect(   btnPause.signal_press, [this]() { mpd.TogglePause();   });
-      miso::connect( btnConsume.signal_press, [this]() { mpd.ToggleConsume(); });
-      miso::connect(  btnRandom.signal_press, [this]() { mpd.ToggleRandom();  });
-      miso::connect(  btnRandom.signal_press, [this]() { mpd.ToggleSingle();  });
-      miso::connect(  btnRandom.signal_press, [this]() { mpd.ToggleRepeat();  });
     }
 
     void Update() { }
