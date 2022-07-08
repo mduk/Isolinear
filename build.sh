5a6b64671a3b4aa185ff431c0809040fb1159134
Daniel Kendell <daniel.kendell@gmail.com>
2022-07-08 21:00:02 +0100

    build dependencies

--- build.sh
+++ build.sh
@@ -2,22 +2,21 @@
 
 set -eu
 
+sudo apt install -y libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-gfx-dev \
+  libx11-xcb-dev libxaw7-dev libxcomposite-dev libxdamage-dev libxft-dev libxkbfile-dev \
+  libxmuu-dev libxres-dev libxtst-dev libxvmc-dev libxcb-* \
+  libasio-dev libfmt-dev \
+  cmake
+
 declare proj="${1:-}"
 
 mkdir -p .build
 cd .build
 
-conan install ..
 cmake ..
 make $proj
 
 if [[ ! -z $proj ]]
 then
-  touchscreen on
-  i3-msg focus output eDP-1 >/dev/null
-
   ./bin/$proj
-
-  touchscreen off
-  i3-msg focus output DP-3 >/dev/null || true
 fi
