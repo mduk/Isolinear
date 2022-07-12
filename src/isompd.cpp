f9ddf67140ebb85b15da6645019ba1986adfbff5
Daniel Kendell <daniel.kendell@gmail.com>
2022-07-12 21:20:51 +0100

    grid

--- src/isompd.cpp
+++ src/isompd.cpp
@@ -46,10 +46,10 @@ int main(int argc, char* argv[])
   isolinear::display::window window(display_position, display_size);
 
   isolinear::grid grid(
-      isolinear::geometry::region(0, 0, display_size.x, display_size.y),
-      window.ButtonFont().Height(), // Row height
-      geometry::vector(10,10),
-      geometry::vector(15,18)
+      { 0, 0, display_size.x, display_size.y },
+      { window.ButtonFont().Height() }, // Row height
+      { 10, 10 },
+      { 15, 18 }
     );
 
 
