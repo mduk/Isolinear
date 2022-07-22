3f2a5d63041f6be5818de45fc71a078d95fd01c7
Daniel Kendell <daniel.kendell@gmail.com>
2022-07-22 12:41:36 +0100

    wip

---
+++ src/testwindows.cpp
@@ -0,0 +1,39 @@
+#include <sstream>
+#include <stdio.h>
+
+#include <SDL2/SDL.h>
+
+#include "init.h"
+#include "display.h"
+#include "ui.h"
+
+
+int main(int argc, char *argv[]) {
+  namespace geometry = isolinear::geometry;
+  namespace pointer = isolinear::pointer;
+
+  auto work_guard = asio::make_work_guard(isolinear::io_context);
+
+  isolinear::init();
+  auto window1 = isolinear::new_window({100, 100}, {500, 500});
+  auto window2 = isolinear::new_window({700, 100}, {500, 500});
+
+  // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+  isolinear::ui::rect rect1{{ 10, 10, 150, 50 }};
+  rect1.colours(isolinear::theme::nightgazer_colours);
+  window1.add(&rect1);
+
+  isolinear::ui::rect rect2{{ 50, 50, 150, 50 }};
+  rect2.colours(isolinear::theme::red_alert_colours);
+  window2.add(&rect2);
+
+  while (isolinear::loop()) {
+    window1.render();
+    window2.render();
+  };
+
+  work_guard.reset();
+  isolinear::shutdown();
+  return 0;
+}
