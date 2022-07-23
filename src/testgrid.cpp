07411f7c286046695e6434f461cb2946ec242855
Daniel Kendell <daniel.kendell@gmail.com>
2022-07-23 09:10:42 +0100

    indicator regions

--- src/testgrid.cpp
+++ src/testgrid.cpp
@@ -27,22 +27,30 @@ int main(int argc, char* argv[])
     );
 
   auto elbo_corner_region = grid.left_columns(4).top_rows(3);
-  auto elbo_vertical_region = grid.left_columns(3).bottom_rows(grid.max_rows() - 4);
-  auto elbo_horizontal_region = grid.top_rows(2).right_columns(grid.max_columns() - 4);
+  auto elbo_vertical_control_region = grid.left_columns(3).bottom_rows(grid.max_rows() - 4);
+  auto elbo_vertical_indicator_region = grid.left_columns(4).right_columns(1).bottom_rows(grid.max_rows() - 4);
+  auto elbo_horizontal_control_region = grid.top_rows(2).right_columns(grid.max_columns() - 4);
+  auto elbo_horizontal_indicator_region = grid.top_rows(3).bottom_rows(0).right_columns(grid.max_columns() - 4);
   auto elbo_content_region = grid.subgrid(5, 4, grid.max_columns(), grid.max_rows());
+
+  isolinear::ui::rect vrect(elbo_vertical_indicator_region.bounds());
+  window.add(&vrect);
+
+  isolinear::ui::rect hrect(elbo_horizontal_indicator_region.bounds());
+  window.add(&hrect);
 
   isolinear::ui::north_west_sweep nwsweep(
       window, elbo_corner_region, {3, 2}, 50, 20
   );
   window.add(&nwsweep);
 
-  isolinear::ui::vertical_button_bar vbbar(window, elbo_vertical_region);
+  isolinear::ui::vertical_button_bar vbbar(window, elbo_vertical_control_region);
     vbbar.add_button("Spoon");
     vbbar.add_button("Knife");
     vbbar.add_button("Fork");
   window.add(&vbbar);
 
-  isolinear::ui::horizontal_button_bar hbbar(window, elbo_horizontal_region);
+  isolinear::ui::horizontal_button_bar hbbar(window, elbo_horizontal_control_region);
     hbbar.add_button("Spoon");
     hbbar.add_button("Knife");
     hbbar.add_button("Fork");
