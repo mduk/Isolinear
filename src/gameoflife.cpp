77a71b1be4d34575cb4e47051429df1e4a9ad5de
Daniel Kendell <daniel.kendell@gmail.com>
2023-10-31 23:08:42 +0000

    ported over better gameoflife implementation

--- src/gameoflife.cpp
+++ src/gameoflife.cpp
@@ -10,151 +10,155 @@ namespace geometry = isolinear::geometry;
 namespace theme = isolinear::theme;
 namespace ui = isolinear::ui;
 
-using game_state = std::vector<bool>;
+class grid_size : public geometry::vector {
+public:
+    grid_size(int x, int y) : vector(x, y) {}
 
-class gameoflife : public isolinear::ui::control {
-protected:
-    geometry::vector m_bigness{67,39};
-    layout::grid m_game_grid;
-    game_state m_state;
-    bool m_paused = false;
-    geometry::vector m_focused_cell{0};
+    int n_cells() {
+      return x * y;
+    }
+};
+
+
+class gameoflife {
+private:
+    using game_state_ptr = std::unique_ptr<bool[]>;
+
+    grid_size m_grid_size;
+    game_state_ptr m_update;
+    game_state_ptr m_display;
 
 public:
-    miso::signal<bool> signal_paused;
-    miso::signal<int, int, int> signal_mouse;
-
-public:
-    gameoflife(isolinear::layout::grid g)
-        : control(g)
-        , m_game_grid(g.bounds(), {25}, {2}, m_bigness, {0})
-        {
+    gameoflife(grid_size gs)
+        : m_grid_size(gs)
+        , m_update(std::make_unique<bool[]>(gs.x*gs.y))
+        , m_display(std::make_unique<bool[]>(gs.x*gs.y))
+    {
       initialise();
     }
 
     void initialise() {
-      m_state.clear();
-      auto cell_count = m_game_grid.cell_count();
-      for (uint16_t index = 1; index <= cell_count; index++) {
-        m_state.push_back(rand() % 6 == 0);
+      for (int i = 0; i < m_grid_size.n_cells(); i++) {
+        m_display[i] = rand() % 6 == 0;
+        m_update[i] = 0;
       }
     }
 
-    int alive_neighbours_of(geometry::vector cell, game_state &state) const {
-      int alive = 0;
-      for (int xo = -1; xo <= 1; xo++) {
-        for (int yo = -1; yo <= 1; yo++) {
-          if (xo == 0 && yo == 0) {
-            continue;
-          }
-          auto i = xytoi( cell.x + xo, cell.y + yo );
-          if (state[i]) {
-            alive++;
-          }
-        }
-      }
-      return alive;
+    geometry::vector neighbour(geometry::vector cell, geometry::vector offset, grid_size gs) {
+      int x = cell.x + offset.x;
+      int y = cell.y + offset.y;
+
+      if (x < 0) { x += gs.x; }
+      if (x > gs.x) { x -= gs.x; }
+      if (y < 0) { y += gs.y; }
+      if (y > gs.y) { y -= gs.y; }
+
+      return {x, y};
     }
 
-    bool cell_update(geometry::vector cell, game_state &read_state) {
-      auto cell_alive = read_state[xytoi(cell)];
-      auto neighbours = alive_neighbours_of(cell, read_state);
-      if (cell_alive && (neighbours < 2 || neighbours > 3)) {
-        return false;
-      }
-      if (!cell_alive && neighbours == 3) {
-        return true;
-      }
-      return cell_alive;
+    std::array<geometry::vector, 8> neighbours_of(geometry::vector cell, grid_size gs) {
+      return {
+          neighbour(cell, {-1, -1}, gs),  // Northwest
+          neighbour(cell, { 0, -1}, gs),  // North
+          neighbour(cell, {+1, -1}, gs),  // Northeast
+
+          neighbour(cell, {-1,  0}, gs),  // West
+          neighbour(cell, {+1,  0}, gs),  // East
+
+          neighbour(cell, {-1, +1}, gs),  // Southwest
+          neighbour(cell, { 0, +1}, gs),  // South
+          neighbour(cell, {+1, +1}, gs),  // Southeast
+      };
+    }
+
+    int xytoi(geometry::vector c, int n_columns) const {
+      return (c.y * n_columns) + c.x;
+    }
+
+    grid_size size() const {
+      return m_grid_size;
+    }
+
+    bool cell_state(geometry::vector c) const {
+      return m_display[xytoi(c, m_grid_size.x)];
     }
 
     void update() {
-      if (m_paused) {
-        return;
-      }
+      for (int cy = 0; cy < m_grid_size.y; cy++) {
+        for (int cx = 0; cx < m_grid_size.x; cx++) {
+          int alive = 0;
+          for (auto neighbour : neighbours_of({cx, cy}, m_grid_size)) {
+            if (m_display[xytoi(neighbour, m_grid_size.x)]) {
+              alive++;
+            }
+          }
 
-      do_update();
-    }
+          int i = xytoi({cx, cy}, m_grid_size.x);
+          bool cell_alive = m_display[i];
 
-    void do_update() {
-      auto cols = m_game_grid.max_columns();
-      auto rows = m_game_grid.max_columns();
+          if (cell_alive && (alive == 2 || alive == 3)) {
+            m_update[i] = true;
+            continue;
+          }
 
-      game_state temp;
-      auto cell_count = m_game_grid.cell_count();
-      for (uint16_t index = 1; index <= cell_count; index++) {
-        temp.push_back(0);
-      }
-      for (int x = 0; x < cols; x++) {
-        for (int y = 0; y < rows; y++) {
-          geometry::vector cell_ref{x, y};
-          temp[xytoi(x, y)] = cell_update(cell_ref, m_state);
+          if (!cell_alive && alive == 3) {
+            m_update[i] = true;
+            continue;
+          }
+
+          m_update[i] = false;
         }
       }
-      m_state = temp;
-    }
 
-    int xytoi(int x, int y) const {
-      return (y * m_game_grid.max_columns()) + x;
-    }
-    int xytoi(geometry::vector position) const {
-      return xytoi(position.x, position.y);
-    }
-
-    void draw(SDL_Renderer* renderer) const {
-      m_game_grid.bounds().fill(renderer, 0xff333333);
-
-      auto focused_cell_i = xytoi(m_focused_cell);
-
-      for (int x = 1; x <= m_game_grid.max_columns(); x++) {
-        for (int y = 1; y <= m_game_grid.max_rows(); y++) {
-          theme::colour cell_colour{0xff000000};
-          auto i = xytoi(x,y);
-          if (m_state[i]) {
-            cell_colour = 0xffffffff;
-          }
-
-          if (i == focused_cell_i) {
-            m_game_grid.cell(x, y).fill(renderer, isolinear::theme::nightgazer_colours.active);
-            m_game_grid.cell(x, y).shrink(5).fill(renderer, cell_colour);
-          }
-          else {
-            m_game_grid.cell(x, y).fill(renderer, cell_colour);
-          }
-        }
-      }
-      SDL_Delay(50);
-    }
-
-    virtual void on_mouse_down(isolinear::event::pointer event) {
-      auto clicked_cell = m_game_grid.cell_at(event.position());
-
-      if (clicked_cell == m_focused_cell) {
-        auto i = xytoi(m_focused_cell);
-        m_state[i] = !m_state[i];
-      }
-      else {
-        m_focused_cell = clicked_cell;
-      }
-
-      auto alive = alive_neighbours_of(m_focused_cell, m_state);
-      emit signal_mouse(m_focused_cell.x, m_focused_cell.y, alive);
-    }
-
-    void pause() {
-      m_paused = !m_paused;
-      emit signal_paused(m_paused);
-    }
-
-    void step() {
-      if (!m_paused) {
-        return;
-      }
-
-      do_update();
+      // We flip-flop between the two bool arrays so that no copying
+      // or moving needs to happen, we just swap pointers.
+      m_display.swap(m_update);
     }
 };
 
+
+class isogameoflife : public isolinear::ui::control {
+protected:
+    std::size_t m_cell_size;
+    gameoflife m_game;
+
+public:
+    isogameoflife(isolinear::layout::grid g)
+    : control(g)
+    , m_cell_size(20)
+    , m_game({
+        static_cast<int>(floor(g.bounds().W()/m_cell_size)),
+        static_cast<int>(floor(g.bounds().H()/m_cell_size))
+    })
+    { }
+
+    void update() {
+      m_game.update();
+    }
+
+    void draw(SDL_Renderer* renderer) const {
+      auto grid_size = m_game.size();
+      for (int cy = 0; cy < grid_size.y; cy++) {
+        for (int cx = 0; cx < grid_size.x; cx++) {
+          int cell_x = (cx * m_cell_size) + m_grid.bounds().origin().x;
+          int cell_y = (cy * m_cell_size) + m_grid.bounds().origin().y;
+          int cell_colour = (
+              m_game.cell_state({cx, cy})
+              ? 0xffffffff
+              : 0xff000000
+          );
+
+          boxColor(
+              renderer,
+              cell_x, cell_y,
+              cell_x + m_cell_size, cell_y + m_cell_size,
+              cell_colour
+          );
+        }
+      }
+      SDL_RenderPresent(renderer);
+    }
+};
 
 int main(int argc, char* argv[]) {
   namespace geometry = isolinear::geometry;
@@ -184,7 +188,7 @@ int main(int argc, char* argv[]) {
   window.add(&hbbar);
 
   isolinear::layout::grid content_area = elbo_layout.content();
-  gameoflife gol(content_area);
+  isogameoflife gol(content_area);
   window.add(&gol);
 
   ui::button &randomise_btn = vbbar.add_button("RANDOMISE");
@@ -192,24 +196,9 @@ int main(int argc, char* argv[]) {
   ui::button &step_btn = vbbar.add_button("STEP");
   step_btn.disable();
 
-  miso::connect(gol.signal_mouse, [&hbbar](int x, int y, int alive){
-    hbbar.label(fmt::format("{}x{} ({})", x, y, alive));
-  });
-  miso::connect(pause_btn.signal_press, [&gol](){ gol.pause(); });
-  miso::connect(randomise_btn.signal_press, [&gol](){ gol.initialise(); });
-  miso::connect(step_btn.signal_press, [&gol](){ gol.step(); });
-
-  miso::connect(gol.signal_paused, [&step_btn](bool paused){
-    if (paused) {
-      step_btn.enable();
-    }
-    else {
-      step_btn.disable();
-    }
-  });
-
   while (isolinear::loop()) {
     gol.update();
+    SDL_Delay(50);
   };
 
   work_guard.reset();
